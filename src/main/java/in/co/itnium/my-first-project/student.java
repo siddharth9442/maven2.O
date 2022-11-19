d53744145ecbaa890f6d0534c97e25b6e774de2a
Siddharth <sidkhubikar21@gmail.com>
2022-11-19 11:18:29 +0530

    #10

---
+++ src/main/java/in/co/itnium/my-first-project/student.java
@@ -0,0 +1,23 @@
+package in.co.itnium.my_first_project;
+
+public class name
+{
+	public void setName(string stud_name)
+	{
+		stud_name = Name;
+		return;
+	}
+}
+
+public class student extends name
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	
+	
+	public Name = siddharth;
+	name n1 = new name();
+	n1.setName(Name);
+	}
+}
---
+++ src/test/java/in/co/itnium/my_first_project/AppTest.java
@@ -0,0 +1,13 @@
+package in.co.itnium.my_first_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
